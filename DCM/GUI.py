f81ccae042aee9565f7634f6f704f275d7631a0d
bartella <bartella@mcmaster.ca>
2022-10-04 12:52:25 -0400

    Commented code, made minor syntax modifications

--- DCM/GUI.py
+++ DCM/GUI.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from cmath import exp
 import json
 from otherfuncs import *
 
@@ -7,23 +5,24 @@ import tkinter
 from ttkthemes import themed_tk as tk
 from tkinter import Button, IntVar, ttk,messagebox, font
 
-#for multilanguage support, https://pypi.org/project/translate/
+#note to self: for multilanguage support, https://pypi.org/project/translate/
 
-#add confirmation message popup to "delete user" section
+#note to self: add confirmation message popup to "delete user" section
 
-def mainPage(): #use ttk notebook for part 2 of project and connected alert thing
+def mainPage():
     clearFrame(masterFrame)
     root.minsize(330, 320)
 
-    notebook = ttk.Notebook(masterFrame) #https://www.tutorialspoint.com/notebook-widget-in-tkinter
+    notebook = ttk.Notebook(masterFrame) #notebook widget for tabs
     notebook.pack(expand=True, fill='both')
 
-    paramTab = ttk.Frame(notebook)
+    paramTab = ttk.Frame(notebook) #parameters tab
     notebook.add(paramTab, text="Parameters")
 
-    mainTab = ttk.Frame(notebook)
+    mainTab = ttk.Frame(notebook) #general tab
     notebook.add(mainTab, text="General")
 
+    #General tab widgets
     connectButton = ttk.Button(
         mainTab, 
         text="Connect",
@@ -34,33 +33,31 @@ def mainPage(): #use ttk notebook for part 2 of project and connected alert thin
         )
     connectButton.pack()
     
-    #Parameters tab
-    with open("parameters.json", "r") as f:
+    #Parameters tab widgets
+    with open("parameters.json", "r") as f: #getting parameter attributes from json file
         print('opened file')
         parameters = json.load(f)
-    #print(parameters)
+    
     row = 1
     for i in parameters:
-        #print(i)
-        p = parameters[i]
+        p = parameters[i] #iterating through a dictionary
         if p != None:
-            #print(p)
-            p["Spinbox"] = ttk.Spinbox(paramTab,
+            p["Spinbox"] = ttk.Spinbox(paramTab, #create spinbox widget
                 from_=p["Range"][0],
                 to=p["Range"][1],
                 increment=p["Inc"]
                 )
             p["Spinbox"].insert(0, p["Default"])
             p["Spinbox"].grid(row=row, column=1, padx=5, pady=5)
-            ttk.Label(paramTab, 
+            ttk.Label(paramTab, #create label widget for spinbox
                 text=p["Name"], 
                 font=("Calibri, 10")
                 ).grid(row=row, column=0, padx=5, pady=5, sticky="w")
             row+=1
-    applyButton = ttk.Button(paramTab, 
+    applyButton = ttk.Button(paramTab, #"apply" button at bottom of page
         text="Apply",
         command=lambda: [
-            getParamVals(parameters),
+            getParamVals(parameters), #collect values inside spinbox widgets (see otherfuncs.py)
             print("Parameters Applied")
             ]
         )
@@ -68,38 +65,38 @@ def mainPage(): #use ttk notebook for part 2 of project and connected alert thin
 
 
 def deleteAccount(username):
-    #enter password for 'username' first
+    #note to self: require enter password for selected username
     print('entered delete account') ##
-    with open("userpass.json", "r") as f:
+    with open("userpass.json", "r") as f: #get usernames and passwords from json file
         print('opened file')
         data = json.load(f)
-    for index, object in enumerate(data):
-        if object['username'] == username:
+    for index, object in enumerate(data): #iterating through login information
+        if object['username'] == username: #searching for selected username
             print('found') ##
-            data.pop(index)
+            data.pop(index) #deleting specified login info
 
-    with open("userpass.json", "w") as f:
-        f.write(json.dumps(data, indent=2))
+    with open("userpass.json", "w") as f: 
+        f.write(json.dumps(data, indent=2)) #updating json file
     
     if not username == '':
-        messagebox.showinfo("Delete user", "User \"{}\" has been deleted.".format(username))
+        messagebox.showinfo("Delete user", "User \"{}\" has been deleted.".format(username)) #confirmation message
     else:
         messagebox.showwarning("Delete user", "No account was selected.")
 
 
 def newAccount(username, password):
     print('entered create account') ##
-    with open("userpass.json", "r") as f:
-        print('opened file')
+    with open("userpass.json", "r") as f: #get existing login info from json file
+        print('opened file') ##
         data = json.load(f)
-    data.append({'username':username, 'password':password})
+    data.append({'username':username, 'password':password}) #adding new username and password
 
-    with open("userpass.json", "w") as f:
-        f.write(json.dumps(data, indent=2))  
-    print('new user created:', username)  
+    with open("userpass.json", "w") as f: 
+        f.write(json.dumps(data, indent=2)) #updating json file
+    print('new user created:', username)  ##
 
 def maxUsersReached():
-    popup = tk.ThemedTk()
+    popup = tk.ThemedTk() #new window
     popup.set_theme(theme)
     popup.title("Max Users Reached!")
     frame = ttk.Frame(popup)
@@ -119,15 +116,17 @@ def maxUsersReached():
     )
     subheader.grid(row=1, column=0)
 
-    selectedUser = tkinter.StringVar(frame)
-    with open("userpass.json", "r") as f:
+    selectedUser = tkinter.StringVar(frame) #initializing selected user var
+    with open("userpass.json", "r") as f: #get login info from json file
         data = json.load(f)
 
-    optionFrame = ttk.Frame(frame)
+    optionFrame = ttk.Frame(frame) #frame to contain account selection
     optionFrame.grid(row=2, column=0)
 
     optionStyle = ttk.Style(optionFrame)
     optionStyle.configure("option.TRadiobutton", font="Calibri 12") ####
+    
+    #iterating through login info to display options
     row=0
     for loginInfo in data:
         user = loginInfo['username']
@@ -141,66 +140,67 @@ def maxUsersReached():
             ).grid(row=row, column=0, sticky='w')
         row+=1
 
-    buttonFrame = ttk.Frame(frame)
+    buttonFrame = ttk.Frame(frame) #frame for delete button
     
     deleteButton = ttk.Button(
         buttonFrame, 
         text="Delete", 
         command=lambda: [
             print("Now deleting:"+selectedUser.get()+"**"), ##
-            deleteAccount(selectedUser.get()),
-            popup.destroy()
+            deleteAccount(selectedUser.get()), #delete selected user (see otherfuncs.py)
+            popup.destroy() #close window
             ]
         )
-    cancelButton = ttk.Button(
+    cancelButton = ttk.Button( #abort deleteAccount()
         buttonFrame,
         text="Cancel",
-        command=lambda: popup.destroy()
+        command=lambda: popup.destroy() #close window
     )
     buttonFrame.grid(column=0, row=3, pady=(2,5))
     deleteButton.grid(column=0, row=0, padx=2, ipadx=7)
     cancelButton.grid(column=1, row=0, padx=2, ipadx=7)
 
 
-def login(userEnter, passEnter):
-    if checkEmptyCredentials(userEnter, passEnter):
+def login(userEnter, passEnter): #login button command
+    if checkEmptyCredentials(userEnter, passEnter): #see otherfuncs.py
         messagebox.showwarning("Login", "Enter a Username and Password.")
         return 
     print('Success') ##
     userPassFound = False
     with open("userpass.json", "r") as f:
-        data = json.load(f)
+        data = json.load(f) #get login info from json file
     for i in data:
-        if i['username'] == userEnter and i['password'] == passEnter:
-            userPassFound = True
+        if i['username'] == userEnter and i['password'] == passEnter: #search for matching username AND password
+            userPassFound = True #login info found
             messagebox.showinfo("Login", "Login Successful!")
-            mainPage()
+            mainPage() #go to mainpage
+            return
     if not userPassFound: 
         messagebox.askretrycancel("Login", "Login Unsuccessful.\nUsername or password not found.\nPlease Try Again.")
 
 
-def createNewUser(userEnter, passEnter):
-    if checkEmptyCredentials(userEnter, passEnter):
+def createNewUser(userEnter, passEnter): #new user button command
+    if checkEmptyCredentials(userEnter, passEnter): #see otherfuncs.py
         messagebox.showwarning("Login", "Enter a Username and Password.")
         return 
     UserAlreadyExists = False
     with open("userpass.json", "r") as f:
-        data = json.load(f)
-    for i in data:
+        data = json.load(f) #get login info from json file
+    for i in data: #iterate through login info to check if user already exists
         if i['username'] == userEnter:
             UserAlreadyExists = True
             messagebox.askretrycancel("New User", "Username already exists!")
-            break
+            return
     if not UserAlreadyExists:
-        if len(data) >= maxUsers:
+        if len(data) >= maxUsers: #ensure max users is not reached
             maxUsersReached()
         else:
-            newAccount(userEnter, passEnter)
+            newAccount(userEnter, passEnter) #append to login info json file
             messagebox.showinfo("New User", "New User Added!")
 
     
-def loginPage():
-    clearFrame(masterFrame)
+def loginPage(): #login page
+    clearFrame(masterFrame) #remove all widgets from frame
     frame = ttk.Frame(masterFrame)
     frame.pack(padx=0, pady=0)
 
@@ -209,49 +209,51 @@ def loginPage():
     userLabel = ttk.Label(frame, text="Username: ", font="Calibri 16")
     passLabel = ttk.Label(frame, text="Password: ", font = "Calibri 16")
 
-    #boxStyle = ttk.Style()
-    #boxStyle.configure("userbox.TEntry", width=500)
-    userBox = ttk.Entry(frame, font="Calibri 12")
-    passBox = ttk.Entry(frame, show="*", font="Calibri 12")
+    userBox = ttk.Entry(frame, font="Calibri 12") #username entry widget
+    passBox = ttk.Entry(frame, show="*", font="Calibri 12") #password entry widget
 
-    buttonFrame = ttk.Frame(frame)
+    buttonFrame = ttk.Frame(frame) #frame for createnewuser and login buttons
 
     style.configure("enter.TButton", font=('Calibri', 12))
-    enter = ttk.Button(
+    loginButton = ttk.Button( #login button
         buttonFrame, 
         text="LOGIN", 
         style="enter.TButton", 
-        command= lambda: login(userBox.get(), passBox.get())
+        command= lambda: login(userBox.get(), passBox.get()) #login function call
     )
-    create = ttk.Button(
+    create = ttk.Button( #create new user button
         buttonFrame,
         text="CREATE NEW",
         style="enter.TButton",
-        command= lambda: createNewUser(userBox.get(), passBox.get())
+        command= lambda: createNewUser(userBox.get(), passBox.get()) #new user function call
     )
     
-    titleLabel.grid(row=0, column=0)
+    titleLabel.grid(row=0, column=0) #inserting widgets
     userLabel.grid(row=1, column=0, sticky='w')
     userBox.grid(row=2, column=0, sticky='w', ipadx=50)
     passLabel.grid(row=3, column=0, sticky='w')
     passBox.grid(row=4, column=0, sticky='w', ipadx=50)
     
     buttonFrame.grid(row=5, column=0)
-    enter.grid(row=0, column=1, pady=(8, 0), padx=(3,0), ipadx=10)
+    loginButton.grid(row=0, column=1, pady=(8, 0), padx=(3,0), ipadx=10)
     create.grid(row=0, column=0, pady=(8, 0), padx=(0,3), ipadx=10)
 
     root.minsize(300, 210)
 
-def welcome(): 
+def welcome(): #welcome page
     frame = ttk.Frame(masterFrame)
     frame.pack(padx=0, pady=0)
 
     titleLabel = ttk.Label(frame, text="Welcome", font="Calibri 25")
     subtitleLabel = ttk.Label(frame, text="3K04 Pacemaker Project", font="Calibri 14")
     style.configure("enter.TButton", font=('Calibri', 12))
-    welcomeButton = ttk.Button(frame, style="welcome.TButton", text="Ok", command=lambda: loginPage())
+    welcomeButton = ttk.Button(frame, 
+        style="welcome.TButton", 
+        text="Ok", 
+        command=lambda: loginPage() #navigate to login page upon button press
+        )
     
-    titleLabel.pack()
+    titleLabel.pack() #inserting widgets
     subtitleLabel.pack(pady=(0, 5))
     welcomeButton.pack(ipadx=20, pady=(0, 4))
 
@@ -264,19 +266,19 @@ def main():
     global masterFrame
     global maxUsers
     global theme
-    maxUsers = 2
+    maxUsers = 10
     #create tkinter window instance
     theme = "scidblue"
     root = tk.ThemedTk()
-    root.set_theme(theme) #breeze, #scidblue
+    root.set_theme(theme) #fitting themes: breeze, scidblue
     root.title("3K04 app")
+    root.iconbitmap("menghi.ico")
     style = ttk.Style()
 
     #create frame to size the window
     masterFrame = ttk.Frame(root)
     masterFrame.pack(fill='both', expand=True)
     welcome()
-    #mainPage()
 
     root.mainloop()
 
--- DCM/otherfuncs.py
+++ DCM/otherfuncs.py
@@ -1,23 +1,23 @@
 from tkinter import messagebox
 
-def clearFrame(frame):
+def clearFrame(frame): #clear all existing widgets from tkinter frame
     try:
-        for i in frame.winfo_children():
+        for i in frame.winfo_children(): #iterating through widgets
             i.destroy()
     except:
         print('**CLEARFRAME ERROR')
 
-def checkEmptyCredentials(username, password):
+def checkEmptyCredentials(username, password): #check if login info is blank
     if username == '' or password == '':
         return True
 
-def getParamVals(parameters):
-    for i in parameters:
+def getParamVals(parameters): #get new parameter values from GUI
+    for i in parameters: #iterating through dictionary
         p = parameters[i]
-        if p != None:
-            cur = p["Spinbox"].get()
-            if float(cur) >= p["Range"][0] and float(cur) <= p["Range"][1]:
-                p["Values"] = p["Spinbox"].get()
+        if p != None: #check that parameter is not blank
+            cur = p["Spinbox"].get() #get current value from GUI spinbox
+            if float(cur) >= p["Range"][0] and float(cur) <= p["Range"][1]: #check if value is in acceptable range
+                p["Values"] = p["Spinbox"].get() #insert new value
             else:
                 messagebox.showwarning("Parameter Editor", "Invalid Value for {}".format(p["Name"]))
-                p["Values"] = None
+                p["Values"] = None #reset current value to noneType
